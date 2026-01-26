63bed74a11070de09d4a4cb6cea454cd65b6e234
DanielSouza2005 <danisimoes741@gmail.com>
2026-01-25 22:35:05 -0300

    Reapply "refactor: implement validation strategies for adoption requests"

--- src/main/java/br/com/alura/adopet/api/validation/ValidacaoTutorPossuiAdocaoAguardandoAvaliacao.java
+++ src/main/java/br/com/alura/adopet/api/validation/ValidacaoTutorPossuiAdocaoAguardandoAvaliacao.java
@@ -2,15 +2,10 @@ package br.com.alura.adopet.api.validation;
 
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
 import br.com.alura.adopet.api.exception.SolicitacaoAdocaoValidacaoException;
-import br.com.alura.adopet.api.model.Adocao;
 import br.com.alura.adopet.api.model.StatusAdocao;
-import br.com.alura.adopet.api.model.Tutor;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
-import br.com.alura.adopet.api.repository.TutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 public class ValidacaoTutorPossuiAdocaoAguardandoAvaliacao implements ValidacaoSolicitacaoAdocao {
@@ -18,17 +13,13 @@ public class ValidacaoTutorPossuiAdocaoAguardandoAvaliacao implements ValidacaoS
     @Autowired
     private AdocaoRepository adocaoRepository;
 
-    @Autowired
-    private TutorRepository tutorRepository;
-
     public void validar(SolicitacaoAdocaoDto solicitacaoAdocaoDto) {
-        List<Adocao> adocoes = adocaoRepository.findAll();
-        Tutor tutor = tutorRepository.getReferenceById(solicitacaoAdocaoDto.idTutor());
+        boolean tutorPossuiAdocaoAguardandoAvaliacao = adocaoRepository.existsByTutorIdAndStatus(
+                solicitacaoAdocaoDto.idTutor(),
+                StatusAdocao.AGUARDANDO_AVALIACAO);
 
-        for (Adocao a : adocoes) {
-            if (a.getTutor() == tutor && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
-                throw new SolicitacaoAdocaoValidacaoException("Tutor já possui outra adoção aguardando avaliação!");
-            }
+        if (tutorPossuiAdocaoAguardandoAvaliacao) {
+            throw new SolicitacaoAdocaoValidacaoException("Tutor já possui outra adoção aguardando avaliação!");
         }
     }
 }
